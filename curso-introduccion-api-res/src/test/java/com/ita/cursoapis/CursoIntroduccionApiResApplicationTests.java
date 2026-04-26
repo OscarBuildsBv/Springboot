644030702be3d5dc0f4a45ff1125fa6f91993425
Oscar <polielbrayan@gmail.com>
2026-04-25 21:49:07 -0500

    Estructura inicial del proyecto Spring Boot

---
+++ curso-introduccion-api-res/src/test/java/com/ita/cursoapis/CursoIntroduccionApiResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita.cursoapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoIntroduccionApiResApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
